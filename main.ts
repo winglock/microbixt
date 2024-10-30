08e7182dbfb948926b304f9c7aec266af9598965
winglock <118047244+winglock@users.noreply.github.com>
2024-10-30 22:14:18 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,28 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        led.stopAnimation()
+        led.plot(0, 0)
+        led.plot(1, 1)
+        led.plot(4, 0)
+        led.plot(3, 1)
+        led.plot(2, 2)
+        led.plot(1, 3)
+        led.plot(3, 3)
+        led.plot(0, 4)
+        led.plot(4, 4)
+    } else {
+        led.stopAnimation()
+        led.plot(1, 0)
+        led.plot(2, 0)
+        led.plot(3, 0)
+        led.plot(0, 1)
+        led.plot(0, 2)
+        led.plot(0, 3)
+        led.plot(1, 4)
+        led.plot(2, 4)
+        led.plot(3, 4)
+        led.plot(4, 1)
+        led.plot(4, 2)
+        led.plot(4, 3)
+    }
+})
